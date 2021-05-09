6b3e63af1c286aceeed3b4eabf444aea590e618a
AlexeyYakovlev1 <alexeyykovlev555@gmail.com>
2021-05-09 13:37:40 +0300

    изменил дизайн radio кнопок

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,12 @@
 import '../styles/style.sass';
+
+const createBuildEndCakeLevel = document.querySelectorAll('.create__build-end-cake-level');
+const createBuildLabelLevel = document.querySelectorAll('.create__build-label-level');
+
+createBuildLabelLevel.forEach(item => {
+  item.addEventListener('click', () => {
+    if (window.getComputedStyle(item, ':before').backgroundColor === 'rgb(238, 112, 122)') {
+      const dataInfo = item.dataset.info;
+    }
+  })
+})
